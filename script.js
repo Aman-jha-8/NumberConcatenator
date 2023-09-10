4a8c2c48cefc93de3985bac71952d6d08cbcaff6
Amanjha8503 <99260113+Amanjha8503@users.noreply.github.com>
2023-09-10 19:21:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+operate= ()=>{
+    var first = document.getElementById("first")
+    var second = document.getElementById("second")
+    var result = document.getElementById("result")
+    // var ans = parseFloat(first.value)+parseFloat(second.value)  //🥴😵‍💫
+    var ans = first.value+second.value
+    result.value=ans;
+}
+
+
+
+
+
